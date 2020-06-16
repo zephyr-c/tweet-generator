4c507162b55e2a77a2b7cc8c868a13fe34408050
vagrant <vagrant@vagrant.vm>
2020-06-16 00:10:00 +0000

    generator working, clunky

--- tweet-gen.py
+++ tweet-gen.py
@@ -1,24 +1,32 @@
 import GetOldTweets3 as got
 import sys
+from random import choice
 
-username = sys.argv[1]
 
 def gettweets(username):
     tweetCriteria = got.manager.TweetCriteria().setUsername(username)\
                                                .setMaxTweets(500)
 
     results = got.manager.TweetManager.getTweets(tweetCriteria)
+    tweets = []
 
-    with open('tweetcorpus.txt', 'w') as file:
-        for tweet in results:
-            file.write(tweet.text)
+    for tweet in results:
+        text = tweet.text.split()
+        tweets.extend(text)
 
-def make_chains(file_path, n):
-    text = open(file_path).read()
+    return tweets
+
+    # with open('tweetcorpus.txt', 'w') as file:
+    #     for tweet in results:
+    #         file.write(tweet.text)
+    #         file.write('\n')
+
+def make_chains(words, n):
+    # text = open(file_path).read()
 
     chains = {}
 
-    words = text.split()
+    # words = text.split()
 
     for i in range(len(words) - (n -1)):
         upper_bound = i + n
@@ -39,10 +47,20 @@ def make_text(chains):
     n = len(link)
     words.extend(link)
 
-    while chains[link]:
+    while chains[link] and len(words) < 30:
         words.append(choice(chains[link]))
 
         link = tuple(words[-n:])
 
 
     return " ".join(words)
+
+username = sys.argv[1]
+
+source = gettweets(username)
+
+chains = make_chains(source, 5)
+
+random_tweet = make_text(chains)
+
+print(random_tweet)
